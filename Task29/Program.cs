39b3d47923d5f3286667194ab8a3843ae924a435
Kosh <mail@ex.com>
2022-07-05 19:47:33 +0300

    Task29 done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] Array(int[] array)
+{
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-1000, 1001);
+    }
+    return array;
+}
+
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int[] numbers = new int[8];
+PrintArr(Array(numbers));
+
+// Console.WriteLine("[{0}]", string.Join(",", Array(numbers))); Печать чётко по заданию, но в строку.
